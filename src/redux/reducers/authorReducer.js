e05bde9767917a87af5a8081465f0d2b03b5ab9e
_rdx98 <elder_louzada@outlook.com>
2020-04-11 18:30:03 -0300

    module 10 | centralizing state

--- src/redux/reducers/authorReducer.js
+++ src/redux/reducers/authorReducer.js
@@ -1,6 +1,9 @@
 import { LOAD_AUTHORS_SUCCESS } from '../actions/actionTypes'
+import initialState from './initialState'
 
-export default function courseReducer(state = [], action) {
+const { authors } = initialState
+
+export default function authorReducer(state = authors, action) {
     switch (action.type) {
         case LOAD_AUTHORS_SUCCESS:
             return action.authors
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,6 +1,9 @@
 import { CREATE_COURSE, LOAD_COURSES_SUCCESS } from '../actions/actionTypes'
+import initialState from './initialState'
 
-export default function courseReducer(state = [], action) {
+const { courses } = initialState
+
+export default function courseReducer(state = courses, action) {
     switch (action.type) {
         case CREATE_COURSE:
             return [...state, { ...action.course }]
