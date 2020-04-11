b6dcdfb09bc80ad2afd71d047de6794333ecd7f8
_rdx98 <elder_louzada@outlook.com>
2020-04-11 12:50:37 -0300

    module 9 | creating action types constants

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,3 +1,5 @@
-export function createCourse(course){
-    return { type: "CREATE_COURSE", course }
+import { CREATE_COURSE } from './actionTypes'
+
+export function createCourse(course) {
+    return { type: CREATE_COURSE, course }
 }
