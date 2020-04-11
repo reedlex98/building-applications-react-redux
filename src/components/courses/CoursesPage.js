edf792bb7678e27195554f220916cf5d255889bc
_rdx98 <elder_louzada@outlook.com>
2020-04-11 00:39:18 -0300

    module 9 | creating first redux action

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,9 +1,29 @@
 import React, { Component } from "react";
 
 class CoursesPage extends Component {
+    state = {
+        course: {
+            title: ""
+        }
+    }
+
+    handleChange = event => {
+        const course = { ...this.state.course, title: event.target.value }
+        this.setState({ course })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+    }
+
     render() {
         return (
-            <h2>Courses</h2>
+            <form onSubmit={this.handleSubmit}>
+                <h2>Courses</h2>
+                <h3>Add Course</h3>
+                <input type="text" onChange={this.handleChange} value={this.state.course.title} />
+                <input type="submit" value="Save" />
+            </form>
         )
     }
 }
