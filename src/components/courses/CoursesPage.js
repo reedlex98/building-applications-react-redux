045802eb5bce03a6e83cbd2107d13dd34f7d9413
_rdx98 <elder_louzada@outlook.com>
2020-04-11 11:20:07 -0300

    module 9 | how redux flow works

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -18,22 +18,27 @@ class CoursesPage extends Component {
     handleSubmit = event => {
         event.preventDefault()
         this.props.dispatch(courseActions.createCourse(this.state.course))
+        this.setState({course: {title:""}})
     }
 
     render() {
+        const {courses} = this.props
+
         return (
             <form onSubmit={this.handleSubmit}>
                 <h2>Courses</h2>
                 <h3>Add Course</h3>
                 <input type="text" onChange={this.handleChange} value={this.state.course.title} />
                 <input type="submit" value="Save" />
+                {courses.map(course => <div key={course.title}>{course.title}</div> )}
             </form>
         )
     }
 }
 
 CoursesPage.propTypes = {
-    dispatch: PropTypes.func.isRequired
+    dispatch: PropTypes.func.isRequired,
+    courses: PropTypes.array.isRequired
 }
 
 function mapStateToProps({ courses }) {
