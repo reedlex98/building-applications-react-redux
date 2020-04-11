a99d0b852c3d01ad5926d7e89fd2dfd8c0875168
_rdx98 <elder_louzada@outlook.com>
2020-04-11 20:45:34 -0300

    module 11 | handling create & update on redux

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,15 +1,23 @@
-import { CREATE_COURSE, LOAD_COURSES_SUCCESS } from './actionTypes'
+import { LOAD_COURSES_SUCCESS, CREATE_COURSE_SUCCESS, UPDATE_COURSE_SUCCESS } from './actionTypes'
 import * as courseApi from '../../api/courseApi'
-
-export function createCourse(course) {
-    return {
-        type: CREATE_COURSE, course
-    }
-}
 
 export function loadCourseSuccess(courses) {
     return {
         type: LOAD_COURSES_SUCCESS,
+        courses
+    }
+}
+
+export function createCourseSuccess(courses) {
+    return {
+        type: CREATE_COURSE_SUCCESS,
+        courses
+    }
+}
+
+export function updateCourseSuccess(courses) {
+    return {
+        type: UPDATE_COURSE_SUCCESS,
         courses
     }
 }
@@ -19,6 +27,22 @@ export function loadCourses() {
         return courseApi.getCourses()
             .then(courses => {
                 dispatch(loadCourseSuccess(courses))
+            })
+            .catch(err => {
+                throw err
+            })
+    }
+}
+
+export function saveCourse(course) {
+    //eslint-disable-next-line no-unused-vars
+    return function (dispatch, getState) {
+        return courseApi
+            .saveCourse()
+            .then(savedCourse => {
+                course.id 
+                ? dispatch(updateCourseSuccess(savedCourse))
+                : dispatch(createCourseSuccess(savedCourse))
             })
             .catch(err => {
                 throw err
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,4 +1,4 @@
-import { CREATE_COURSE, LOAD_COURSES_SUCCESS } from '../actions/actionTypes'
+import { CREATE_COURSE, LOAD_COURSES_SUCCESS, CREATE_COURSE_SUCCESS, UPDATE_COURSE_SUCCESS } from '../actions/actionTypes'
 import initialState from './initialState'
 
 const { courses } = initialState
@@ -9,6 +9,10 @@ export default function courseReducer(state = courses, action) {
             return [...state, { ...action.course }]
         case LOAD_COURSES_SUCCESS:
             return action.courses
+        case CREATE_COURSE_SUCCESS:
+            return [...state, { ...action.course }]
+        case UPDATE_COURSE_SUCCESS:
+            return state.map(course => course.id === action.course.id ? action.course : course)
         default:
             return state
     }
