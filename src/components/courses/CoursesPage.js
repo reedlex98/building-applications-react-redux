1be524fedc714713d8212ef9a7e44cb429983fe4
_rdx98 <elder_louzada@outlook.com>
2020-04-11 15:43:59 -0300

    module 10 | configuring load courses thunk

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -2,37 +2,18 @@ import React, { Component } from "react";
 import { connect } from 'react-redux'
 import * as courseActions from '../../redux/actions/courseActions'
 import PropTypes from 'prop-types'
-import {bindActionCreators} from 'redux'
+import { bindActionCreators } from 'redux'
 
 class CoursesPage extends Component {
-    state = {
-        course: {
-            title: ""
-        }
-    }
-
-    handleChange = event => {
-        const course = { ...this.state.course, title: event.target.value }
-        this.setState({ course })
-    }
-
-    handleSubmit = event => {
-        event.preventDefault()
-        this.props.actions.createCourse(this.state.course)
-        this.setState({ course: { title: "" } })
-    }
 
     render() {
         const { courses } = this.props
 
         return (
-            <form onSubmit={this.handleSubmit}>
+            <>
                 <h2>Courses</h2>
-                <h3>Add Course</h3>
-                <input type="text" onChange={this.handleChange} value={this.state.course.title} />
-                <input type="submit" value="Save" />
                 {courses.map((course, i) => <div key={i}>{course.title}</div>)}
-            </form>
+            </>
         )
     }
 }
--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,5 +1,27 @@
-import { CREATE_COURSE } from './actionTypes'
+import { CREATE_COURSE, LOAD_COURSES_SUCCESS } from './actionTypes'
+import * as courseApi from '../../api/courseApi'
 
 export function createCourse(course) {
-    return { type: CREATE_COURSE, course }
+    return {
+        type: CREATE_COURSE, course
+    }
+}
+
+export function loadCourseSuccess(courses) {
+    return {
+        type: LOAD_COURSES_SUCCESS,
+        courses
+    }
+}
+
+export function loadCourses() {
+    return function (dispatch) {
+        return courseApi.getCourses()
+            .then(courses => {
+                dispatch(loadCourseSuccess(courses))
+            })
+            .catch(err => {
+                throw err
+            })
+    }
 }
