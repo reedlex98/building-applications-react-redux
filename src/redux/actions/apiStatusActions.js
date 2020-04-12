aba3056b4ab21b174bc254900b608627166a084f
_rdx98 <elder_louzada@outlook.com>
2020-04-12 15:14:00 -0300

    module 11 | api status calls

---
+++ src/redux/actions/apiStatusActions.js
@@ -0,0 +1,7 @@
+import {BEGIN_API_CALL} from './actionTypes'
+
+export function beginApiCall(){
+    return {
+        type: BEGIN_API_CALL
+    }
+}
---
+++ src/redux/reducers/apiStatusReducer.js
@@ -0,0 +1,14 @@
+import { BEGIN_API_CALL } from '../actions/actionTypes'
+import initialState from './initialState'
+
+function actionTypeEndsInSuccess(type) {
+    return type.substring(type.length - 8) === "_SUCCESS"
+}
+
+export default function authorReducer(state = initialState.apiCallsInProgress, action) {
+    if (action.type === BEGIN_API_CALL) {
+        return state + 1
+    } else if (actionTypeEndsInSuccess(action.type)){
+        return state - 1
+    }
+}
